172286ca3945128c7dd6fa206db53f66c65616df
Maxim Shapovalov <syndex@list.ru>
2024-12-08 21:36:45 +0300

    Initial commit

---
+++ src/main/java/syndexmx/github/com/musicstruct/MusicstructApplication.java
@@ -0,0 +1,13 @@
+package syndexmx.github.com.musicstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicstructApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicstructApplication.class, args);
+	}
+
+}
---
+++ src/test/java/syndexmx/github/com/musicstruct/MusicstructApplicationTests.java
@@ -0,0 +1,13 @@
+package syndexmx.github.com.musicstruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicstructApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
